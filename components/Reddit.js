c9612137f0501e05c545f60c44057d1132f2a4ea
shadowwzw <miditab@yandex.ru>
2017-07-23 21:45:17 +0400

    fix

--- components/Reddit.js
+++ components/Reddit.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, ActivityIndicator, Platform, FlatList, Image, TouchableOpacity } from 'react-native';
 
-export default ({ currentTime, defaultIcon, openInWebViewComponent, item : { data: { thumbnail, title, subreddit_name_prefixed, created_utc, num_comments, url } } }) => (
+export default ({ currentTime, defaultIcon, redditHost, openInWebViewComponent, item : { data: { thumbnail, title, subreddit_name_prefixed, created_utc, num_comments, url, permalink } } }) => (
   <View style={{flex: 1, flexDirection: 'row', marginBottom: 5, marginTop: 5}}>
-    <Image
-      style={{width: 100, height: 100}}
-      source={{uri: thumbnail.match(/jpg/) ? thumbnail : defaultIcon }}
-    />
+    <TouchableOpacity onPress={() => openInWebViewComponent(url, title)}>
+      <Image
+        style={{width: 100, height: 100}}
+        source={{uri: thumbnail.match(/jpg/) ? thumbnail : defaultIcon }}
+      />
+    </TouchableOpacity>
     <View style={{flex: 1, backgroundColor: 'white'}} >
       <TouchableOpacity onPress={() => openInWebViewComponent(url, title)}>
         <Text style={{fontWeight: 'bold'}}>{title || ""}</Text>
