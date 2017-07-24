15b01a48c3f8eb8efe171905af531c59295fad44
shadowwzw <miditab@yandex.ru>
2017-07-25 00:14:21 +0400

    remove console.log

--- sagas/subreddits.js
+++ sagas/subreddits.js
@@ -6,10 +6,8 @@ const getSubredditsFetch = function* (action){
   yield put({ type: GET_SUBREDDITS.START });
   try {
     const url = `https://www.reddit.com/subreddits.json?count=${count || 0}${after ? `&after=${after}` : ""}`;
-    console.log('url = ', url);
     const result = yield fetch(url);
     const json = yield result.json();
-    console.log('json = ', json);
     yield put({ type: GET_SUBREDDITS.FINISH, data: json.data.children, after: json.data.after });
   } catch (e) {
     yield put({ type: GET_SUBREDDITS.ERROR, error: e.message });
